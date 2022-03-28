72b7dc6a8fadc0ecb31e1be3f5bba9ccf6bc015c
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-03-27 23:46:48 -0400

    feat: set-up icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,32 @@
-import Image from 'next/image';
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+const socialMediaIcons = [
+  "icon-facebook",
+  'icon-instagram',
+  'icon-linkedin',
+  'icon-twitter',
+];
+
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </a>
+
+      <ul>
+        {socialMediaIcons.map((iconClass) => (
+          <li key={iconClass} className={iconClass}></li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
