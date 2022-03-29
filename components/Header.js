6c2d09739e75d70154c0556d88fac1ac7033ad9d
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-03-28 23:45:23 -0400

    feat(header): intial work

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,38 @@
+import classNames from "classnames";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 import ThemeToggle from "./ThemeToggle";
 
+import styles from "../styles/Header.module.css";
+
 export default function Header() {
   return (
-    <nav className="container">
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/jobs">Jobs</Link>
-        </li>
-        <li>
-          <Link href="/getting-hired">Getting hired</Link>
-        </li>
-        <li>
-          <Link href="/resources">Resources</Link>
-        </li>
-      </ul>
-      <ThemeToggle />
+    <nav className={classNames(styles.wrapper, "container-fluid position-relative")}>
+      <div className="container d-flex justify-content-between align-items-center h-100">
+        <label htmlFor="test" className={styles.label}>
+          <span className="icon-menu d-lg-none"></span>
+        </label>
+        <input id="test" className={styles.testCheck} type="checkbox" />
+        <p>
+          Remote<span>JS</span>Jobs
+        </p>
+        <ul className={styles.links}>
+          <li className="me-5 mb-4 mb-lg-0">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="me-5 mb-4 mb-lg-0">
+            <Link href="/jobs">Jobs</Link>
+          </li>
+          <li className="me-5 mb-4 mb-lg-0">
+            <Link href="/getting-hired">Getting hired</Link>
+          </li>
+          <li className="me-5 mb-4 mb-lg-0">
+            <Link href="/resources">Resources</Link>
+          </li>
+        </ul>
+        <ThemeToggle />
+      </div>
     </nav>
   );
 }
