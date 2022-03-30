863917a38312d627e37f786163aaa5171c9c008a
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-03-29 23:00:17 -0400

    fix(Header): fix the colors of the links

--- components/Header.js
+++ components/Header.js
@@ -8,30 +8,36 @@ import styles from "../styles/Header.module.css";
 
 export default function Header() {
   return (
-    <nav className={classNames(styles.wrapper, "container-fluid position-relative")}>
+    <nav
+      className={classNames(
+        styles.wrapper,
+        "container-fluid position-relative"
+      )}
+    >
       <div className="container d-flex justify-content-between align-items-center h-100">
         <label htmlFor="test" className={styles.label}>
           <span className="icon-menu d-lg-none"></span>
         </label>
         <input id="test" className={styles.testCheck} type="checkbox" />
-        <p>
-          Remote<span>JS</span>Jobs
+        <p className={styles.logo}>
+          <Link href="/">
+            RemoteJSJobs
+          </Link>
         </p>
-        <ul className={styles.links}>
-          <li className="me-5 mb-4 mb-lg-0">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="me-5 mb-4 mb-lg-0">
-            <Link href="/jobs">Jobs</Link>
-          </li>
-          <li className="me-5 mb-4 mb-lg-0">
-            <Link href="/getting-hired">Getting hired</Link>
-          </li>
-          <li className="me-5 mb-4 mb-lg-0">
-            <Link href="/resources">Resources</Link>
-          </li>
-        </ul>
-        <ThemeToggle />
+        <div className="d-flex align-self-end align-items-center h-100">
+          <ul className={styles.links}>
+            <li className="me-5 mb-4 mb-lg-0">
+              <Link href="/jobs">Jobs</Link>
+            </li>
+            <li className="me-5 mb-4 mb-lg-0">
+              <Link href="/getting-hired">Getting hired</Link>
+            </li>
+            <li className="me-5 mb-4 mb-lg-0">
+              <Link href="/resources">Resources</Link>
+            </li>
+          </ul>
+          <ThemeToggle />
+        </div>
       </div>
     </nav>
   );
