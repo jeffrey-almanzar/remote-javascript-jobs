37e2e5ef112903627949f07dae559f9002e60658
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-04 22:28:39 -0400

    feat(getting-hired.js): initialize tabs

--- pages/getting-hired.js
+++ pages/getting-hired.js
@@ -1,11 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+function Tab() {
+  return (
+    <div className="container">
+      <ul className="nav nav-tabs">
+        <li className="nav-item col-4 text-center">
+          <a className="nav-link active bg-yellow" aria-current="page" href="#">
+            Active
+          </a>
+        </li>
+        <li className="nav-item col-4 text-center">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item col-4 text-center">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default function GettingHired() {
   return (
-    <div className="container">
-      Getting hired
-    </div>
-  )
+    <Tab />
+  );
 }
