ae0d9f8a922056e8810ff428fbf7c914103c6c09
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-10 12:25:44 -0400

    feat(homepage): add latest jobs section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import classNames from "classnames";
 
@@ -7,6 +8,9 @@ import image from "../public/images/homepageHero.gif";
 import standout from "../public/images/standout.png";
 import competence from "../public/images/competence.png";
 import negotiation from "../public/images/negotiation.png";
+
+import { jobs } from "../components/JobBoard/data";
+import JobListing from "../components/JobBoard/JobListing";
 
 function Hero(props) {
   return (
@@ -19,9 +23,9 @@ function Hero(props) {
             <p className="mb-4 lead">
               The most reliable remote JavaScript jobs on the internet.
             </p>
-            <a className="btn btn-warning" href="/jobs">
-              Show me the jobs
-            </a>
+            <p className="btn btn-warning">
+              <Link href="/jobs">Show me the jobs</Link>
+            </p>
           </div>
           <div>
             <Image src={image} />
@@ -32,54 +36,80 @@ function Hero(props) {
   );
 }
 
+function GettingHiredSection(props) {
+  return (
+    <div className="container py-5">
+      <h2>Getting hired</h2>
+      <div className="d-md-flex justify-content-between">
+        <div className="col-md-3 mt-5 text-center">
+          <Image height={250} width={250} src={standout} />
+          <h4 className="mb-2">Standing out</h4>
+          <p>
+            Are you applying and not getting interviews? Assuming you don’t have
+            much experience, you…
+          </p>
+          <p className="mt-3">
+            <a href="#" className={styles.cta}>
+              Learn more
+            </a>
+          </p>
+        </div>
+        <div className="col-md-3 mt-5 text-center">
+          <Image height={250} width={250} src={competence} />
+          <h4 className="mb-2">Demonstrating competence</h4>
+          <p>
+            Are you applying and not getting interviews? Assuming you don’t have
+            much experience, you…
+          </p>
+          <p className="mt-3">
+            <a href="#" className={styles.cta}>
+              Learn more
+            </a>
+          </p>
+        </div>
+        <div className="col-md-3 mt-5 text-center">
+          <Image height={250} width={250} src={negotiation} />
+          <h4 className="mb-2">Handling offers</h4>
+          <p>
+            Are you applying and not getting interviews? Assuming you don’t have
+            much experience, you…
+          </p>
+          <p className="mt-3">
+            <a href="#" className={styles.cta}>
+              Learn more
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CallToAction(props) {
+  const { title = "View all the latest jobs", url = "/jobs" } = props;
+  return (
+    <p className={classNames(styles.sectionCallToAction, "mt-4 text-center")}>
+      <Link href={url}>{title}</Link>
+    </p>
+  );
+}
+
+function JobListingSection(props) {
+  return (
+    <div className="container">
+      <h2 className="mb-5">Latest jobs</h2>
+      <JobListing jobs={jobs} />
+      <CallToAction />
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="mb-5">
       <Hero />
-      <div className="container pt-5">
-        <h2>Getting hired</h2>
-        <div className="d-md-flex justify-content-between">
-          <div className="col-md-3 mt-5 text-center">
-            <Image height={250} width={250} src={standout} />
-            <h4 className="mb-2">Standing out</h4>
-            <p>
-              Are you applying and not getting interviews? Assuming you don’t
-              have much experience, you…
-            </p>
-            <p className="mt-3">
-              <a href="#" className={styles.cta}>
-                Learn more
-              </a>
-            </p>
-          </div>
-          <div className="col-md-3 mt-5 text-center">
-            <Image height={250} width={250} src={competence} />
-            <h4 className="mb-2">Demonstrating competence</h4>
-            <p>
-              Are you applying and not getting interviews? Assuming you don’t
-              have much experience, you…
-            </p>
-            <p className="mt-3">
-              <a href="#" className={styles.cta}>
-                Learn more
-              </a>
-            </p>
-          </div>
-          <div className="col-md-3 mt-5 text-center">
-            <Image height={250} width={250} src={negotiation} />
-            <h4 className="mb-2">Handling offers</h4>
-            <p>
-              Are you applying and not getting interviews? Assuming you don’t
-              have much experience, you…
-            </p>
-            <p className="mt-3">
-              <a href="#" className={styles.cta}>
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
+      <GettingHiredSection />
+      <JobListingSection />
     </div>
   );
 }
