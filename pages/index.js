d2f9d9370744f64498ebbbe412968f1f71e0483b
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-10 17:29:31 -0400

    feat(homepage): make job categories responsive

--- pages/index.js
+++ pages/index.js
@@ -108,28 +108,28 @@ function JobCategoriesSection (props) {
   return (
     <div className="container">
       <h2 className="mb-5">Popular jobs</h2>
-      <ul className="d-flex justify-content-between flex-wrap">
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+      <ul className="d-md-flex justify-content-between flex-wrap">
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-native mb-3")}></span>
           <span>React Jobs</span>
         </li>
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-vue mb-3")}></span>
           <span>Category name</span>
         </li>
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-angular mb-3")}></span>
           <span>Category name</span>
         </li>
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-jquery mb-3")}></span>
           <span>Category name</span>
         </li>
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-node mb-3")}></span>
           <span>Category name</span>
         </li>
-        <li className={classNames("col-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
+        <li className={classNames("col-md-4 d-flex flex-column justify-content-center text-center", styles.jobCategoryItem)}>
           <span className={classNames(styles.jobCategoryIcon, "icon-native mb-3")}></span>
           <span>Category name</span>
         </li>
