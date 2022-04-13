20d12a3c727f2fb304d3bb1cbd8c7e6885066469
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-12 21:22:36 -0400

    feat(resources): add dummy content

--- pages/resources.js
+++ pages/resources.js
@@ -2,10 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import TableOfContent from '../components/Blog/TableOfContent';
+
+
 export default function Resources() {
   return (
     <div className="container">
-      Resources
+      <h1>Resources</h1>
+      <TableOfContent />
+
+      <div className='mb-5'>
+        <h3 className='mb-3'>Test 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+      <div className='mb-5'>
+        <h3 className='mb-3'>Test 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+      <div className='mb-5'>
+        <h3 className='mb-3'>Test 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+      <div className='mb-5'>
+        <h3 className='mb-3'>Test 4</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
     </div>
   )
 }
