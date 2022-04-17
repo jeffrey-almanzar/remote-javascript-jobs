c2ebb5ed61cb0905e5f41be732697e0f0378631f
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-17 13:39:00 -0400

    feat(jobs): initialize job details page

--- pages/jobs/[slug].js
+++ pages/jobs/[slug].js
@@ -1,7 +1,53 @@
-export default function Test() {
+import _ from "lodash";
+import { jobs } from "../../components/JobBoard/data";
+import HTMLParser from "../../components/HTMLParser";
+
+export default function Test({ jobPost }) {
+  const {
+    title,
+    company_name,
+    date,
+    employment_type,
+    development_type,
+    experience_level,
+    salary,
+    technologies,
+    description,
+    apply_link,
+    logo_url,
+    company_site,
+  } = jobPost;
+
+  if (_.isEmpty(jobPost)) {
     return (
-        <div>
-            <h1>Test 2</h1>
-        </div>
+      <div className="container">
+        <h1>Sorry, page not found</h1>
+      </div>
     );
+  }
+
+  return (
+    <div className="container py-5">
+      <h1 className="pb-4">{title}</h1>
+      <div>
+        <HTMLParser>{description}</HTMLParser>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const jobId = _.get(context, "query.slug").split("-")[0];
+  // const { query } = context;
+  // const hasFilters = !_.isEmpty(query);
+
+  // const filteredJobs = [];
+  console.log(jobId);
+  const job = jobs.find((job) => job.id == jobId);
+
+  return {
+    props: {
+      jobPost: job || null,
+    }, // will be passed to the page component as props
+  };
 }
