b5d07603fadba7e6b0af0e89f4959296ef655a11
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-04-22 21:46:20 -0400

    fix(blog): fix hover states

---
+++ components/JobBoard/CompanyCard.js
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import classNames from "classnames";
+import DropDown from "./Dropdown";
+
+import styles from "../../styles/JobBoard.module.css";
+
+export default function CompanyCard({ options }) {
+  const router = useRouter();
+  return (
+    <div className={classNames(styles.filters, "p-4 sticky-top")}>
+      <h3 className="mb-3">Filters</h3>
+      <ul>
+        {options.map((option) => (
+          <li key={option} className="mb-2">
+            <DropDown {...option} />
+          </li>
+        ))}
+      </ul>
+      <p
+        onClick={() => router.push({ pathname: "/jobs" })}
+        className="btn btn-link"
+      >
+        Reset filters
+      </p>
+    </div>
+  );
+}
